3c10ba0648eb6a00b69739f2f70b68524e2b56c6
DvirBitan123 <dvirbitan123455@gmail.com>
2024-01-22 09:23:50 +0200

    navBar sign out deletes userDetails success

--- frontend/src/app/components/NavBar.tsx
+++ frontend/src/app/components/NavBar.tsx
@@ -9,13 +9,8 @@ import classNames from '../utils/ClassNames';
 const navigation = [
   { name: 'Images', href: ROUTES.HOME, current: false },
   { name: 'map', href: ROUTES.MAP, current: false },
-  { name: 'My Page', href: ROUTES.USER, current: false },
-
+  { name: 'My Page', href: ROUTES.USER, current: false }
 ];
-
-// function classNames(...classes: string[]) {
-//   return classes.filter(Boolean).join(' ')
-// }
 
 export default function NavBar() {
   const [userEmail, setUserEmail] = useAtom(userEmailAtom);
@@ -69,18 +64,17 @@ export default function NavBar() {
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 font-medium text-sm">
                 <a
-                  href='/Login'
+                  href={ROUTES.LOGIN}
                   className='mr-4 rounded-md px-3 py-2 text-gray-300 ease-out duration-200 hover:bg-fuchsia-500 hover:text-white'
                 >
                   Login
                 </a>
                 <a
                   className='text-gray-300 rounded-md px-3 py-2 ease-out duration-200 hover:bg-purple-500 hover:text-white'
-                  href='/Register'
+                  href={ROUTES.REGISTER}
                 >
                   Sign Up
                 </a>
-                {/* </div> */}
                 <Menu as="div" className="relative ml-3">
                   <div>
                     <Menu.Button className="relative flex rounded-full text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
@@ -112,11 +106,10 @@ export default function NavBar() {
                       <Menu.Item>
                         {({ active }) => (
                           <a
-                            href="#"
+                            href={ROUTES.HOME}
                             className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                             onClick={() => {
                               localStorage.setItem('images_token', '')
-                              localStorage.setItem('user_email', '')
                             }}
                           >
                             Sign out
